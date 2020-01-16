33cd20cbd8fbced3d38e6c2584cc4f5a5a0314e2
Lee <Talamortis@gmail.com>
2020-01-16 22:06:32 +0000

    Update Module SQL
    Updated module SQL so users can add comments to each config option

--- sql/updates/world/2020_01_16_module_config.sql
+++ sql/updates/world/2020_01_16_module_config.sql
@@ -4,20 +4,20 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-DROP TABLE IF EXISTS `module_config`;
 CREATE TABLE IF NOT EXISTS `module_config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `config` varchar(50) DEFAULT NULL,
   `value` varchar(50) DEFAULT NULL,
+  `comment` longtext,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 DELETE FROM `module_config`;
 /*!40000 ALTER TABLE `module_config` DISABLE KEYS */;
-INSERT INTO `module_config` (`id`, `config`, `value`) VALUES
-	(1, 'modsample.enableHelloWorld', '0'),
-	(2, 'modsample.stringtest', 'String is working! :D'),
-	(3, 'modsample.intTest', '1908');
+INSERT INTO `module_config` (`id`, `config`, `value`, `comment`) VALUES
+	(1, 'modsample.enableHelloWorld', '0', NULL),
+	(2, 'modsample.stringtest', 'String is working! :D', NULL),
+	(3, 'modsample.intTest', '1908', NULL);
 /*!40000 ALTER TABLE `module_config` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
